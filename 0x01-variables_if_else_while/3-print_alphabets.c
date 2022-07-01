aa295a76ab75a91b83ce962d20f370839a425708
briannanjeri <briannanjeri2020@gmail.com>
2022-07-01 19:45:59 +0300

    file 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,13 +4,11 @@ int main(void)
 {
 char ch;
 for(ch='a'; ch<='z'; ch++){
-putchar(ch);
+putchar(ch) + "\n";
 }
 for(ch='A'; ch<='Z'; ch++){
-putchar(ch);
+putchar(ch) +"\n";
 }
-putchar("\n");
-
 return 0;
 
 }
