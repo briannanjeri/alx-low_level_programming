03e1cb80954f27c69388e51c73646263bd6a070e
briannanjeri <107076291+briannanjeri@users.noreply.github.com>
2022-11-06 21:26:09 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,15 +5,13 @@
  * Return: capitalzied string
 */
 
-char *string_toupper(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] <= 'z' && str[i] >= 'a')
-			str[i] -= 32;
-	}
-	return (str);
+char *string_toupper(char *str){
+    int i;
+    for(i=0; str[i]!='\0'; i++){
+        if(str[i]>='a' && str[i]<='z'){
+            str[i]=str[i]-32;
+        }
+    }
+    return(str);
 }
 
