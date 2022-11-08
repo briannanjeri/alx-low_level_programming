3e7109a7a438a27d6a918b3b7b4ca2f52ab1ede6
briannanjeri <briannanjeri2020@gmail.com>
2022-11-08 21:39:56 +0300

    initial commit

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,3 @@
-#ifndef HEADERFILE_H
-#define HEADERFILE_H
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -7,5 +5,3 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-char **strtow(char *str);
-#endif
